9d2f38a071af12765e5e22d91c51e01efde71ffe
Valentin Rabanelly <valentin@aircall.io>
2018-11-22 15:49:25 +0100

    Fix linter warning

--- src/App/index.js
+++ src/App/index.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <img src={avatar} />
+        <img src={avatar} alt="Avatar" />
       </div>
     );
   }
